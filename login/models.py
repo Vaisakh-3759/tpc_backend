648039fe48d71a4a86235116773a437222f4444b
Vaisakh <vaisakhrn@gmail.com>
2024-04-26 22:00:57 +0530

    Update 26/4

--- login/models.py
+++ login/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import AbstractUser, Group, Permission
 import uuid
 
-class Users(AbstractUser):
+class Users(models.Model):
     username = models.CharField(max_length=255)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(max_length=255, unique=True, verbose_name=_('Email Address'))
@@ -12,24 +12,24 @@ class Users(AbstractUser):
     last_name = models.CharField(max_length=255, verbose_name=_('Last Name'))
     is_superuser = models.BooleanField(default=False)
     last_login = models.DateTimeField(auto_now_add=True)
-    made_password = models.CharField(max_length = 500, null = False)
+    made_password = models.CharField(max_length=500, null=False)
     backlogs = models.IntegerField(default=0)
     gpa = models.FloatField(default=0)
     backlog_history = models.BooleanField(default=False)
     groups = models.ManyToManyField(Group, related_name='users_custom', blank=True)
     user_permissions = models.ManyToManyField(Permission, related_name='users_custom', blank=True)
 
-
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
     def save(self, *args, **kwargs):
         if not self.pk:
-            self.set_password(self.made_password)
-        super(Users, self).save(*args, **kwargs) 
-        
+            self.password = make_password(self.made_password)
+        super(Users, self).save(*args, **kwargs)
+
     def __str__(self):
         return self.email
+
 class Admins(models.Model):
     user = models.OneToOneField(Users, on_delete=models.CASCADE)
     is_superadmin = models.BooleanField(default=False)
