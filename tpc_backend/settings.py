2d871216078e0b66d673725408e26071868ae8aa
Vaisakh <vaisakhrn@gmail.com>
2024-04-27 11:59:34 +0530

    Update 27/4

--- tpc_backend/settings.py
+++ tpc_backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'corsheaders.middleware.CorsMiddleware',
     "login",
     "Password",
     "Drives",
@@ -142,6 +143,10 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
 
 
 # Static files (CSS, JavaScript, Images)
